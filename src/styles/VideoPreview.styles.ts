dbbcef93de1590557b69c8c81caae7d5283e6a4f
2sick2 <toonit772@gmail.com>
2025-05-11 19:56:35 +0900

    비디오 미리보기 추가

---
+++ src/styles/VideoPreview.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginHorizontal: 5,
+    marginBottom: 20,
+    width: 150,
+  },
+  thumbnail: {
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+    marginBottom: 5,
+  },
+});
